e70c1288ef478fe0eb1310b870561596e2e37989
Nurjamal <n.mirbaizaeva@gmail.com>
2022-11-28 12:36:01 +0600

    2

--- homework 4/main.swift
+++ homework 4/main.swift
@@ -1,11 +1,58 @@
-//
-//  main.swift
-//  homework 4
-//
-//  Created by Nurjamal Mirbaizaeva on 28/10/22.
-//
+////№1.  Используя условные операторы, функции и циклы создать следующую программу:
+var twoString = ""
+for num in 1...100{
+    if num % 2 == 0{
+        twoString += "\(num),"
+    }
+}
+print(twoString)
 
-import Foundation
+var threeString = ""
+for num in 1...100 where num % 3 == 0{
+    threeString += "\(num),"
+}
+print(threeString)
 
-print("Hello, World!")
+var fourString = ""
+for num in 1...100{
+    if num % 4 == 0{
+        fourString += "\(num),"
+    }
+}
+print(fourString)
 
+var fiveString = ""
+for num in 1...100 where num % 5 == 0{
+    fiveString += "\(num),"
+}
+print(fiveString)
+ 
+//#2 Используя условные операторы, функции и циклы создать следующую программу: Записать в одну переменную типа String 20 разных слов (с разной длиной) через запятую. После того, как созданная ранее функция примет как аргумент созданную строку - вернет следующее (например) - “Яблоко - 6 символов \n Морковь - 7 символов” и т.д до последнего слова
+
+var num = ["Мука", "Сливочное Масло", "Банан", "Рыба", "Зелень",  "Торт", "Чизкейк", "Мясо", "Хлеб", "Сыр", "Огурцы", "Помидор", "Курица", "Яблоко", "Груша", "Ананас", "Манго", "Сливки", "Рис", "Кукуруз"]
+var str = ""
+for i in num{
+    var hasEscape = false
+    var escapeCount = 0
+    var symbolCount = 0
+    var hasSymbols = false
+    for item in i{
+        if item == " "{
+            hasEscape = true
+            escapeCount += 1
+        }else if item == "," || item == ":" || item == "."{
+            hasSymbols = true
+            symbolCount += 1
+        }
+    }
+    str += "\(i) - \(i.count - escapeCount - symbolCount) символов"
+    if hasEscape == true{
+        str += "и \(escapeCount) пробелов"
+    }
+    if hasSymbols == true{
+        str += " и \(symbolCount) знаков препинания"
+    }
+    str += "\n"
+}
+
+print(str)
